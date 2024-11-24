789ce98ae9bc668a4e4c90194da02ce6e377890d
Alex <alexanderdiallo78@gmail.com>
2024-11-24 13:08:22 +0000

    Simple Changfe

--- GUI/main.py
+++ GUI/main.py
@@ -1,5 +1,22 @@
+""" Main Modul zum GUI starten"""
 
+def fib(start: int) -> float:
+    """
+    Fibonacci recursiv berechnen
+
+    Args:
+        start (int): der Wert der Fibonacci Folge
+
+    Returns:
+        float: Das Ergebnis der Fib reihe
+    """
+    if start == 0:
+        return 0
+    if start == 1:
+        return 1
+    return fib(start-1) + fib(start-2)
 
 if __name__ == "__main__":
     print("Hello world")
+    print(f"Fib von 10: {fib(10)}")
     input("..")
